d7cd080f464ab2f41e4f61bda56989e7e0c3f254
Justin Mott <jmott5@byu.edu>
2022-02-12 09:00:32 -0700

    Welcome page view, table creation updates, dummy data insert

---
+++ dummyData.sql
@@ -0,0 +1,13 @@
+INSERT INTO player (username, password, email, admin) VALUES ('mattm', 'toothyMaw', 'thursday@xhorhas.net', true), 
+('ObiKenobi','password', 'hellothere@naboo.gov', false)('WillingBlam', 'werewolves', 'makingmyway@email.com', false);
+INSERT INTO campaign (joincode, campaignname, dmuserid) VALUES (122, 'Eberron', 1), (66, 'Mandolorian', 2);
+
+INSERT INTO character (ac, hp, initiativebonus, charactername, level, userid, campaignid) 
+VALUES (16, 55, 3, 'Bardy McBard', 5, 2, 1),
+(17, 192, 0, 'Fjord Stone', 16, 3, 1);
+
+INSERT INTO monster (ac, initiativebonus, monstername, cr, public) VALUES (25, 22, 'Tarrasque', 17, TRUE);
+INSERT INTO encounter (campaignid, 'Regular Gnolls') VALUES (1);
+
+INSERT INTO fight (monsterid, encounterid) VALUES (1,1);
+INSERT INTO playin (userid, campaignid) VALUES (2, 1);
